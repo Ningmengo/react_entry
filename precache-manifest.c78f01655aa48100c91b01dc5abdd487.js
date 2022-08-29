ae978bab6b628b5c8bdfcba0b1f9a3d145001003
Long Guan <2668084184gn@gmail.com>
2022-08-29 16:15:51 -0400

    Updates

---
+++ precache-manifest.c78f01655aa48100c91b01dc5abdd487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2e9965fb198329c44b58a51294d8d5",
+    "url": "/counter_react/index.html"
+  },
+  {
+    "revision": "320bb6daaab84cd7e140",
+    "url": "/counter_react/static/css/main.c6cc0205.chunk.css"
+  },
+  {
+    "revision": "c4102a1751d61c8531a0",
+    "url": "/counter_react/static/js/2.85c97319.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/counter_react/static/js/2.85c97319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "320bb6daaab84cd7e140",
+    "url": "/counter_react/static/js/main.46a2959f.chunk.js"
+  },
+  {
+    "revision": "77e01df31ffbd6071fc9",
+    "url": "/counter_react/static/js/runtime-main.ca4a4332.js"
+  }
+]);
