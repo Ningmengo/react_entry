c98e0ec9c769b2bc302cb600afc9b88e725e7ac9
Long Guan <2668084184gn@gmail.com>
2022-08-30 18:00:30 -0400

    message

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -6,30 +6,52 @@ const UseStateCounter = () => {
     setValue(value + 1);
   };
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
   return (
     <>
-      <section style={{ margin: "4rem 0" }}></section>
-      <h2>regular counter</h2>
-      <h1>{value}</h1>
-      <button
-        className="btn"
-        onClick={() => {
-          setValue(value - 1);
-        }}
-      >
-        decrease
-      </button>
-      <button
-        className="btn"
-        onClick={() => {
-          setValue(0);
-        }}
-      >
-        reset
-      </button>
-      <button className="btn" onClick={increaseMethod}>
-        increase
-      </button>
+      <div>
+        <section style={{ margin: "4rem 0" }}></section>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button
+          className="btn"
+          onClick={() => {
+            setValue(value - 1);
+          }}
+        >
+          decrease
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            setValue(0);
+          }}
+        >
+          reset
+        </button>
+        <button className="btn" onClick={increaseMethod}>
+          increase
+        </button>
+      </div>
+      <div className="counterComplex">
+        <h2>complex counter</h2>
+        <h1>{value}</h1>
+        <button
+          className="btn"
+          onClick={() => {
+            complexIncrease();
+          }}
+        >
+          increase later
+        </button>
+      </div>
     </>
   );
 };
